67415f26f324b4b113c364b390242606e0459157
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2025-05-02 19:07:38 -0400

    add environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,3 @@
-// export const environment = {
-//   backendApi: 'http://localhost:3000/gpt',
-//   assistantApi: 'http://localhost:3000/sam-assistant',
-// };
 export const environment = {
   backendApi: 'https://nestai-production.up.railway.app/gpt',
   assistantApi: 'https://nestai-production.up.railway.app/sam-assistant',
