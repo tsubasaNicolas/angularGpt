2699372047493fca97f188e658ca0fda9caa6c25
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2025-05-02 00:02:58 -0400

    add node server.js 8

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Servir los archivos estáticos de Angular desde el directorio 'dist/angular-gpt'
 app.use(express.static(path.join(__dirname, "dist/angular-gpt")));
 
-// Redirigir todas las solicitudes a 'index.html'
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/angular-gpt/index.html"));
+  res.sendFile(path.join(__dirname, "dist/angular-gpt", "index.html"));
 });
 
-// Escuchar en el puerto especificado
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// 🚨 Asegúrate de escuchar en 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
