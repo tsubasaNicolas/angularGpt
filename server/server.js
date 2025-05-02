73bb702f9886a9e1f0924ee61777cb1ecd30c8c3
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2025-05-02 01:00:35 -0400

    add serverjs in server folder 2

--- server/server.js
+++ server/server.js
@@ -6,20 +6,20 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const distPath = path.join(__dirname, "../dist/angular-gpt");
-const indexHtml = path.join(distPath, "index.html");
-
-if (!fs.existsSync(indexHtml)) {
-  console.error("❌ ERROR: index.html not found at:", indexHtml);
-} else {
-  console.log("✅ index.html found at: encontrado", indexHtml);
-}
+const indexPath = path.join(distPath, "index.html");
 
 app.use(express.static(distPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(indexHtml);
+  res.sendFile(indexPath);
 });
 
+if (!fs.existsSync(indexPath)) {
+  console.error("❌ ERROR: index.html not found at:", indexPath);
+} else {
+  console.log("✅ index.html found at:", indexPath);
+}
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+  console.log(`🚀 Server is running on http://0.0.0.0:${PORT}`);
 });
