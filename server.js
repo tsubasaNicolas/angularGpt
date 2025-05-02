5ded2f095f0d128d917c2aaa30be687de22b56d3
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2025-05-01 23:21:26 -0400

    add node server.js server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "dist/angular-gpt")));
 
+// Este * debe ir al final y sin parámetros raros
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/angular-gpt/index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado en puerto ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
